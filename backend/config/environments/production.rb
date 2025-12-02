ea15d253bed6f9097a2f47c66055f980f86a4f42
Adam <ashaw315@gmail.com>
2025-12-02 17:26:49 -0500

    Cookies fixes

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.action_dispatch.cookies_same_site_protection = :none
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,10 +1,4 @@
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store,
-    key: '_mysms_messenger_session',
-    same_site: :none,
-    secure: true,
-    domain: 'mysms-messenger-0b5d.onrender.com'
-else
-  Rails.application.config.session_store :cookie_store,
-    key: '_mysms_messenger_session'
-end
+Rails.application.config.session_store :cookie_store,
+  key: '_mysms_backend_session',
+  same_site: :none,
+  secure: Rails.env.production?
